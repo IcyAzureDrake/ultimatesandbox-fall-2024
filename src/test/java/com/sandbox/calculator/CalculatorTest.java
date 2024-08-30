59a85279eb2ab023f5e4baed6b25b868f35a2543
IcyAzureDrake <cal2160956@maricopa.edu>
2024-08-29 22:33:34 -0700

    Calculator and Calculatortest

---
+++ src/test/java/com/sandbox/calculator/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.sandbox.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(10, Calculator.add(5,5));
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
